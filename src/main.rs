0798c1483ba4cd5b7ba9d55acff9eef0ffc455b7
Jo_Kastro <joaquimcastro2909@gmail.com>
2023-03-08 11:37:54 -0300

    Chapter 12.3 finished

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::env;
+use std::{
+	env,
+	process,
+};
+
+use rust_grep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = env::args().collect();
 
-    let  query: &String = &args[1];
-    let file_path: &String = &args[2];
-    
-    println!("Searching for: {}", query);
-    println!("In file: {}", file_path);
+	let config: Config = Config::build(&args).unwrap_or_else( |err: &str| {
+		println!("Problem parsing arguments: {}", err);
+		process::exit(1);
+	});
+
+	println!("Searching for: {}", config.query);
+	println!("In file: {}", config.file_path);
+
+	if let Err(e) = rust_grep::run(config) {
+		println!("Application error: {}", e);
+		process::exit(1);
+	}
 }
