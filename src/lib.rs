a6d97e9f41684afdb17b1a43fa9ff552d6d60c68
Jo_Kastro <joaquimcastro2909@gmail.com>
2023-03-10 18:55:04 -0300

    rust_grep MVP

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,90 @@
 use std::{
-    fs,
-    error::Error,
+	fs,
+	error::Error,
 };
 
-pub struct Config {
-	pub query: String,
-	pub file_path: String,
-}
+pub mod structs;
 
-impl Config {
-	pub fn build(args: &[String]) -> Result<Config, &'static str> {
-		if args.len() < 3 {
-			return Err("Not enough arguments");
-		}
+pub fn run(config: structs::Config) -> Result< (), Box<dyn Error>> {
+	let contents: String = fs::read_to_string(config.file_path)?;
 
-		let  query: String = args[1].clone();
-		let file_path: String = args[2].clone();
+	let results = if config.ignore_case {
+		search_case_insensitive(&config.query, &contents)
+	} else {
+		search(&config.query, &contents)
+	};
 
-		Ok(Config {query, file_path})
+	for line in results {
+		println!("{line}");
 	}
-}
-
-pub fn run(config: Config) -> Result< (), Box<dyn Error>> {
-	let content: String = fs::read_to_string(config.file_path)?;
-
-	println!("With text: \n{}", content);
 
 	Ok( ())
+}
+
+pub fn search<'a>(
+	query: &str, 
+	contents: &'a str
+) -> Vec<&'a str> {
+
+	let mut results: Vec<&str> = Vec::new();
+	
+	for line in contents.lines() {
+		if line.contains(query) {
+			results.push(line);
+		}
+	}
+
+	results
+}
+
+pub fn search_case_insensitive<'a>(
+	query: &str,
+	contents: &'a str
+) -> Vec<&'a str> {
+
+	let query: String = query.to_lowercase();
+	let mut results: Vec<&str> = Vec::new();
+	
+	for line in contents.lines() {
+		if line.to_lowercase().contains(&query) {
+			results.push(line);
+		}
+	}
+
+	results
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn case_sensitive() {
+		let query = "duct";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+		assert_eq!(
+			vec!["safe, fast, productive."],
+			search(query, contents)
+		);
+	}
+
+	#[test]
+	fn case_insensitive() {
+		let query = "rUsT";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+		assert_eq!(
+			vec!["Rust:", "Trust me."],
+			search_case_insensitive(query, contents)
+		);
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,18 @@ use std::{
 	process,
 };
 
-use rust_grep::Config;
+use rust_grep::structs;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
-	let config: Config = Config::build(&args).unwrap_or_else( |err: &str| {
-		println!("Problem parsing arguments: {}", err);
+	let config: structs::Config = structs::Config::build(&args).unwrap_or_else( |err: &str| {
+		eprintln!("Problem parsing arguments: {err}");
 		process::exit(1);
 	});
 
-	println!("Searching for: {}", config.query);
-	println!("In file: {}", config.file_path);
-
 	if let Err(e) = rust_grep::run(config) {
-		println!("Application error: {}", e);
+		eprintln!("Application error: {e}");
 		process::exit(1);
 	}
 }
